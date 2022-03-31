df1978be94ebd4a16632bf00c1695fca52ca6172
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-03-31 12:48:35 -0400

    initial commit

---
+++ pages/nft/[id].tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function NFTDropPage() {
+  return (
+    <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
+      <div className="bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4">
+        <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
+          <div className="bg-gradient-to-br from-yellow-400 to-purple-600 p-2 rounded-xl">
+            <img
+              className="w-44 rounded-xl object-cover lg:h-96 lg:w-72"
+              src="https://links.papareact.com/8sg"
+              alt=""
+            />
+          </div>
+
+          <div className="p-5 text-center space-y-2">
+            <h1 className="text-4xl font-bold text-white">Papafam</h1>
+            <h2 className="text-gray-300">jcosidappdoqdpqwdqpwdpqwojpjdpqjdpqdpwpodvfvsdlvjs;d</h2>
+          </div>
+        </div>
+      </div>
+      <div>
+        {/*header */}
+        
+        {/*content */}
+        {/*button */}
+      </div>
+    </div>
+  )
+}
+
+export default NFTDropPage

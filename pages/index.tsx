f42134645630a0ee4708bdf098c9d7c201da0a96
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-04-07 10:02:21 -0400

    styled home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { sanityClient } from '../sanity'
+import Link from 'next/link'
+import { sanityClient, urlFor } from '../sanity'
 import { Collection } from '../typings'
 
 interface Props {
@@ -16,6 +17,32 @@ const Home = ({ collections }: Props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>welcome to the nft drop challenge</h1>
+      <header className="flex items-center justify-between">
+        <h2 className="sm:w-18 w-52 cursor-pointer text-xl font-extralight">
+          the papafam nft marketplace
+        </h2>
+      </header>
+      <main className="bg-slate-100 p-10 shadow-xl shadow-rose-400/20">
+        <div className="grid space-x-3 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+          {collections.map((collection) => (
+            <Link href={`/nft/${collection.slug.current}`} key={collection._id}>
+              <div className="flex cursor-pointer flex-col items-center transition-all duration-200 hover:scale-105">
+                <img
+                  className="h-96 w-60 rounded-2xl object-cover"
+                  src={urlFor(collection.mainImage).url()}
+                  alt=""
+                />
+                <div className="p-5">
+                  <h2 className="text-cc_black text-3xl">{collection.title}</h2>
+                  <p className="text-cc_light_gray mt-2 text-sm">
+                    {collection.description}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </main>
     </div>
   )
 }
